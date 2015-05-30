6752468e919ec3fbb0fe205ce00cf9a72679f85e
cakin <catkin_nice@163.com>
2015-05-30 14:27:28 +0800

    修改建库SQL

--- src/main/resources/DB/super_massage.sql
+++ src/main/resources/DB/super_massage.sql
@@ -4,63 +4,53 @@
 -- ------------------------------------------------------
 -- Server version	5.6.13
 
---
--- Table structure for table `t_order`
---
+DROP DATABASE IF EXISTS super_massage;
 
-DROP TABLE IF EXISTS `t_order`;
-CREATE TABLE `t_order` (
-  `id` bigint(20) NOT NULL COMMENT '订单ID',
-  `store_id` bigint(20) NOT NULL COMMENT '商店ID',
-  `user_id` bigint(20) NOT NULL COMMENT '客户ID',
-  `total_store_price` float NOT NULL COMMENT '订单店铺价格',
-  `total_app_price` float NOT NULL COMMENT '订单APP价格',
-  `pay_id` varchar(100) NOT NULL DEFAULT '' COMMENT '支付单号（在线支付使用）',
-  `pay_type` bit(1) NOT NULL DEFAULT b'0' COMMENT '支付类型（0：到店支付，1：在线支付）',
-  `pay_status` bit(1) NOT NULL DEFAULT b'0' COMMENT '支付状态（0：未支付，1：已支付）',
-  PRIMARY KEY (`id`),
-  KEY `FK_order_store` (`store_id`),
-  KEY `FK_order_user` (`user_id`),
-  CONSTRAINT `FK_order_store` FOREIGN KEY (`store_id`) REFERENCES `t_store` (`id`) ON DELETE NO ACTION,
-  CONSTRAINT `FK_order_user` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`) ON DELETE NO ACTION
+CREATE DATABASE super_massage DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE super_massage;
+
+DROP TABLE IF EXISTS `t_store`;
+CREATE TABLE `t_store` (
+  `id` bigint(20) NOT NULL COMMENT '店铺ID',
+  `name` varchar(50) NOT NULL COMMENT '店铺名称',
+  `pwd` varchar(50) NOT NULL DEFAULT '' COMMENT '店铺管理密码',
+  `long_lat_itude` varchar(50) NOT NULL DEFAULT '' COMMENT '店铺经纬度',
+  `address` varchar(200) NOT NULL DEFAULT '' COMMENT '店铺地址',
+  `phone` varchar(15) NOT NULL DEFAULT '' COMMENT '电话',
+  `remark` varchar(500) NOT NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`id`)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `t_order`
---
 
 
---
--- Table structure for table `t_order_item`
---
-
-DROP TABLE IF EXISTS `t_order_item`;
-CREATE TABLE `t_order_item` (
-  `id` bigint(20) NOT NULL COMMENT '订单项ID',
-  `package_id` bigint(20) NOT NULL COMMENT '商品ID',
-  `plan_staff_id` bigint(20) DEFAULT NULL COMMENT '预约员工ID',
-  `used_staff_id` bigint(20) DEFAULT NULL COMMENT '服务员工ID',
-  `plan_time` time DEFAULT NULL COMMENT '预约时间',
-  `used_time` time DEFAULT NULL COMMENT '消费时间',
+DROP TABLE IF EXISTS `t_store_buy`;
+CREATE TABLE `t_store_buy` (
+  `id` bigint(20) NOT NULL COMMENT '购买ID',
+  `store_id` bigint(20) NOT NULL COMMENT '店铺ID',
+  `staff_num` tinyint(4) NOT NULL DEFAULT '0' COMMENT '员工数量',
+  `price` float NOT NULL COMMENT '购买价格',
+  `buy_time` time NOT NULL COMMENT '购买时间',
+  `start_time` time NOT NULL COMMENT '服务开始时间',
+  `end_time` time NOT NULL COMMENT '服务结束时间',
   PRIMARY KEY (`id`),
-  KEY `FK_orderItemUsed_staff` (`used_staff_id`),
-  KEY `FK_orderItemPlan_staff` (`plan_staff_id`),
-  KEY `FK_orderItem_packages` (`package_id`),
-  CONSTRAINT `FK_orderItemPlan_staff` FOREIGN KEY (`plan_staff_id`) REFERENCES `t_staff` (`id`) ON DELETE NO ACTION,
-  CONSTRAINT `FK_orderItemUsed_staff` FOREIGN KEY (`used_staff_id`) REFERENCES `t_staff` (`id`) ON DELETE NO ACTION,
-  CONSTRAINT `FK_orderItem_packages` FOREIGN KEY (`package_id`) REFERENCES `t_packages` (`id`) ON DELETE NO ACTION
+  KEY `FK_storeBuy_store` (`store_id`),
+  CONSTRAINT `FK_storeBuy_store` FOREIGN KEY (`store_id`) REFERENCES `t_store` (`id`) ON DELETE NO ACTION
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `t_order_item`
---
 
 
---
--- Table structure for table `t_packages`
---
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL COMMENT '用户ID',
+  `account` varchar(20) NOT NULL COMMENT '用户帐号',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '用户昵称',
+  `sex` bit(1) NOT NULL DEFAULT b'0' COMMENT '性别（男：0，女：1）',
+  `age` tinyint(4) NOT NULL DEFAULT '-1' COMMENT '年龄',
+  `icon` varchar(50) NOT NULL DEFAULT '' COMMENT '用户头像',
+  `pwd` varchar(50) NOT NULL DEFAULT '' COMMENT '用户密码',
+  `bank_id` varchar(50) NOT NULL DEFAULT '' COMMENT '支付帐号',
+  `remark` varchar(100) NOT NULL DEFAULT '' COMMENT '个性签名',
+  PRIMARY KEY (`id`)
+);
 
 DROP TABLE IF EXISTS `t_packages`;
 CREATE TABLE `t_packages` (
@@ -75,16 +65,7 @@ CREATE TABLE `t_packages` (
   KEY `FK_packages_store` (`store_id`),
   CONSTRAINT `FK_packages_store` FOREIGN KEY (`store_id`) REFERENCES `t_store` (`id`) ON DELETE CASCADE
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `t_packages`
---
-
-
---
--- Table structure for table `t_staff`
---
 
 DROP TABLE IF EXISTS `t_staff`;
 CREATE TABLE `t_staff` (
@@ -102,16 +83,7 @@ CREATE TABLE `t_staff` (
   KEY `FK_staff_store` (`store_id`),
   CONSTRAINT `FK_staff_store` FOREIGN KEY (`store_id`) REFERENCES `t_store` (`id`) ON DELETE CASCADE
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `t_staff`
---
-
-
---
--- Table structure for table `t_staff_packages`
---
 
 DROP TABLE IF EXISTS `t_staff_packages`;
 CREATE TABLE `t_staff_packages` (
@@ -122,78 +94,39 @@ CREATE TABLE `t_staff_packages` (
   CONSTRAINT `FK_sp_packages` FOREIGN KEY (`package_id`) REFERENCES `t_packages` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_sp_staff` FOREIGN KEY (`staff_id`) REFERENCES `t_staff` (`id`) ON DELETE CASCADE
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `t_staff_packages`
---
 
 
---
--- Table structure for table `t_store`
---
-
-DROP TABLE IF EXISTS `t_store`;
-CREATE TABLE `t_store` (
-  `id` bigint(20) NOT NULL COMMENT '店铺ID',
-  `name` varchar(50) NOT NULL COMMENT '店铺名称',
-  `pwd` varchar(50) NOT NULL DEFAULT '' COMMENT '店铺管理密码',
-  `long_lat_itude` varchar(50) NOT NULL DEFAULT '' COMMENT '店铺经纬度',
-  `address` varchar(200) NOT NULL DEFAULT '' COMMENT '店铺地址',
-  `phone` varchar(15) NOT NULL DEFAULT '' COMMENT '电话',
-  `remark` varchar(500) NOT NULL DEFAULT '' COMMENT '备注',
-  PRIMARY KEY (`id`)
-);
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `t_store`
---
-
-
---
--- Table structure for table `t_store_buy`
---
-
-DROP TABLE IF EXISTS `t_store_buy`;
-CREATE TABLE `t_store_buy` (
-  `id` bigint(20) NOT NULL COMMENT '购买ID',
-  `store_id` bigint(20) NOT NULL COMMENT '店铺ID',
-  `staff_num` tinyint(4) NOT NULL DEFAULT '0' COMMENT '员工数量',
-  `price` float NOT NULL COMMENT '购买价格',
-  `buy_time` time NOT NULL COMMENT '购买时间',
-  `start_time` time NOT NULL COMMENT '服务开始时间',
-  `end_time` time NOT NULL COMMENT '服务结束时间',
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order` (
+  `id` bigint(20) NOT NULL COMMENT '订单ID',
+  `store_id` bigint(20) NOT NULL COMMENT '商店ID',
+  `user_id` bigint(20) NOT NULL COMMENT '客户ID',
+  `total_store_price` float NOT NULL COMMENT '订单店铺价格',
+  `total_app_price` float NOT NULL COMMENT '订单APP价格',
+  `pay_id` varchar(100) NOT NULL DEFAULT '' COMMENT '支付单号（在线支付使用）',
+  `pay_type` bit(1) NOT NULL DEFAULT b'0' COMMENT '支付类型（0：到店支付，1：在线支付）',
+  `pay_status` bit(1) NOT NULL DEFAULT b'0' COMMENT '支付状态（0：未支付，1：已支付）',
   PRIMARY KEY (`id`),
-  KEY `FK_storeBuy_store` (`store_id`),
-  CONSTRAINT `FK_storeBuy_store` FOREIGN KEY (`store_id`) REFERENCES `t_store` (`id`) ON DELETE NO ACTION
+  KEY `FK_order_store` (`store_id`),
+  KEY `FK_order_user` (`user_id`),
+  CONSTRAINT `FK_order_store` FOREIGN KEY (`store_id`) REFERENCES `t_store` (`id`) ON DELETE NO ACTION,
+  CONSTRAINT `FK_order_user` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`) ON DELETE NO ACTION
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `t_store_buy`
---
 
 
---
--- Table structure for table `t_user`
---
-
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user` (
-  `id` bigint(20) NOT NULL COMMENT '用户ID',
-  `account` varchar(20) NOT NULL COMMENT '用户帐号',
-  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '用户昵称',
-  `sex` bit(1) NOT NULL DEFAULT b'0' COMMENT '性别（男：0，女：1）',
-  `age` tinyint(4) NOT NULL DEFAULT '-1' COMMENT '年龄',
-  `icon` varchar(50) NOT NULL DEFAULT '' COMMENT '用户头像',
-  `pwd` varchar(50) NOT NULL DEFAULT '' COMMENT '用户密码',
-  `bank_id` varchar(50) NOT NULL DEFAULT '' COMMENT '支付帐号',
-  `remark` varchar(100) NOT NULL DEFAULT '' COMMENT '个性签名',
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `t_order_item`;
+CREATE TABLE `t_order_item` (
+  `id` bigint(20) NOT NULL COMMENT '订单项ID',
+  `package_id` bigint(20) NOT NULL COMMENT '商品ID',
+  `plan_staff_id` bigint(20) DEFAULT NULL COMMENT '预约员工ID',
+  `used_staff_id` bigint(20) DEFAULT NULL COMMENT '服务员工ID',
+  `plan_time` time DEFAULT NULL COMMENT '预约时间',
+  `used_time` time DEFAULT NULL COMMENT '消费时间',
+  PRIMARY KEY (`id`),
+  KEY `FK_orderItemUsed_staff` (`used_staff_id`),
+  KEY `FK_orderItemPlan_staff` (`plan_staff_id`),
+  KEY `FK_orderItem_packages` (`package_id`),
+  CONSTRAINT `FK_orderItemPlan_staff` FOREIGN KEY (`plan_staff_id`) REFERENCES `t_staff` (`id`) ON DELETE NO ACTION,
+  CONSTRAINT `FK_orderItemUsed_staff` FOREIGN KEY (`used_staff_id`) REFERENCES `t_staff` (`id`) ON DELETE NO ACTION,
+  CONSTRAINT `FK_orderItem_packages` FOREIGN KEY (`package_id`) REFERENCES `t_packages` (`id`) ON DELETE NO ACTION
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `t_user`
---
