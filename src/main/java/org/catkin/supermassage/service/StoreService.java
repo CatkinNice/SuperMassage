5cc0f0af592265b5403135784bc93fe891a45f8c
cakin <catkin_nice@163.com>
2015-06-02 22:27:43 +0800

    Signed-off-by: cakin <catkin_nice@163.com>

--- src/main/java/org/catkin/supermassage/service/StoreService.java
+++ src/main/java/org/catkin/supermassage/service/StoreService.java
@@ -2,21 +2,30 @@ package org.catkin.supermassage.service;
 
 import org.catkin.supermassage.entity.Store;
 import org.catkin.supermassage.repository.StoreRepository;
+import org.catkin.supermassage.utils.Sequence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * 
- * @author cakin
+ * @author Catkin_nice
  *
  */
 @Service
 public class StoreService {
 	@Autowired
-	private StoreRepository storeRepository;
+	private StoreRepository sr;
+	
+	public Store addOrEditStore(Store store) {
+		if (store.getId() == null) {
+			store.setId(Sequence.getNextId());
+		}
+		sr.addOrEditStore(store);
+		return store;
+	}
 
 	public Store getStoreById(Long id) {
 		
-		return storeRepository.getStoreById(id);
+		return sr.getStoreById(id);
 	}
 }
---
+++ src/main/java/org/catkin/supermassage/utils/ControllerAspect.java
@@ -0,0 +1,84 @@
+package org.catkin.supermassage.utils;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Aspect
+@Component
+public class ControllerAspect {
+
+	@Around("within(org.catkin.supermassage.controller.*Controller)")
+	public Object around(ProceedingJoinPoint jp) {
+		Log.info(getLogInfo(jp));
+		Object result = null;
+		try {
+			result = jp.proceed();
+		} catch (LogicException e) {
+			Log.error(e);
+			throw e;
+		} catch (Throwable t) {
+			Log.error(t);
+		}
+		return result;
+	}
+
+	private String getLogInfo(JoinPoint jp) {
+		StringBuilder info = new StringBuilder("RequestUrl:");
+		try {
+			// 方法名
+			String methodName = jp.getSignature().getName();
+			// 类
+			Class<?> clz = jp.getSignature().getDeclaringType();
+			// @RequestMapping注解
+			RequestMapping mapping =  clz.getAnnotation(RequestMapping.class);
+			if (mapping != null && mapping.value().length > 0) {
+				info.append(mapping.value()[0]);
+			}
+			
+			// 参数
+			String param = ", param:";
+			Object [] args = jp.getArgs();
+			Class<?>[] clzs = new Class<?>[args.length];
+			
+			for (int i = 0; i < args.length; i++) {
+				if (args[i] != null) {
+					clzs[i] = args[i].getClass();
+					param += args[i] + ",";
+				} else {
+					clzs[i] = Serializable.class;
+					param += "null,";
+				}
+			}
+
+			// 方法
+			Method method = clz.getMethod(methodName, clzs);
+			mapping =  method.getAnnotation(RequestMapping.class);
+			
+			if (mapping != null) {
+				if (mapping.value().length > 0) {
+					info.append(mapping.value()[0]);
+				}
+				
+				if (mapping.method().length > 0) {
+					info.append(", method:").append(mapping.method()[0]);
+				}
+			}
+			
+			if (args.length > 0) {
+				info.append(param);
+				info.deleteCharAt(info.length() - 1);
+			}
+		} catch (Exception ex) {
+			Log.error(ex);
+		}
+		return info.toString();
+	}
+	
+}
